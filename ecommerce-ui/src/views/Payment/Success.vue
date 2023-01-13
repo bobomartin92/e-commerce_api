f6d2788f898166c0e2600af6734a4476a3cdf9c3
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2023-01-13 17:55:54 +0100

    fixes for integration of stripe checkout in the frontend

--- ecommerce-ui/src/views/Payment/Success.vue
+++ ecommerce-ui/src/views/Payment/Success.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="alert alert-success" role="alert">
+  <div class="alert alert-success" role="alert" id="message">
     Payment successful
   </div>
 </template>
 
 <script>
+const axios = require('axios')
 export default {
   name:'PaymentSuccess',
   props:["baseURL"],
@@ -15,10 +16,22 @@ export default {
     }
   },
   methods:{
-
+    saveOrder() {
+      axios.post(this.baseURL+"order/add/?token="+this.token+"&sessionId="+this.sessionId)
+          .then(()=>{
+            // change message to order placed
+            console.log("Success");
+            document.getElementById("message").innerHTML = "order placed";
+            //router.push()
+          }).catch((error)=>{
+        console.log(error);
+      })
+    }
   },
   mounted(){
-
+    this.token = localStorage.getItem("token");
+    this.sessionId = localStorage.getItem("sessionId");
+    this.saveOrder()
   }
 }
 </script>
